77ad4793dfbd800b79eebf4ea9eee10b3d094d1d
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2025-10-22 23:52:10 +0530

    update: create movie endpoint added

--- server/VStreamServer/main.go
+++ server/VStreamServer/main.go
@@ -15,8 +15,10 @@ func main() {
 		c.String(200, "Hello, VStream movies")
 	})
 
+	// movies router
 	router.GET("/movies", controllers.GetMovies())
 	router.GET("/movie/:imdb_id", controllers.GetMovie())
+	router.POST("/addmovie", controllers.AddMovie())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
